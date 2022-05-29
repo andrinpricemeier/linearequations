0e62916bb0c499e44d53f2f98e4a18fdd9eeb94d
Andrin Meier <andrin@pricemeier.ch>
2022-05-29 21:15:10 +0200

    Add bottom action bar
    The bottom bar is meant to trigger the solving of the equation.

---
+++ webapp/app/components/BottomActionBar.stories.tsx
@@ -0,0 +1,7 @@
+import { BottomActionBar } from "./BottomActionBar";
+export default {
+  title: "Bottom Action Bar",
+  component: BottomActionBar,
+};
+
+export const Primary = () => <BottomActionBar />;
---
+++ webapp/app/components/BottomActionBar.tsx
@@ -0,0 +1,9 @@
+import { Button } from "./Button";
+
+export const BottomActionBar = () => {
+  return (
+    <div className="flex justify-end">
+      <Button content="Solve" onClick={() => {}} />
+    </div>
+  );
+};
