30b0bb5166ebd12d572e5df2c950631e5239cdf3
Andrin Meier <andrin@pricemeier.ch>
2022-05-30 08:04:59 +0200

    Add loader overlay

--- webapp/app/components/MainApp.tsx
+++ webapp/app/components/MainApp.tsx
@@ -8,8 +8,10 @@ import { SolutionVariableList } from "./SolutionVariableList";
 import { SolvingFailedText } from "./SolvingFailedText";
 import { Title } from "./Title";
 import { TopActionBar } from "./TopActionBar";
+import LoadingOverlay from "react-loading-overlay-ts";
 
 export const MainApp = () => {
+  const [isSolving, setIsSolving] = useState(true);
   const [dimension, setDimension] = useState<number>(2);
 
   const increaseDimension = useCallback(() => {
@@ -27,13 +29,18 @@ export const MainApp = () => {
         <Button content="-" onClick={decreaseDimension} />
         <Button content="+" onClick={increaseDimension} />
       </TopActionBar>
-      <CoefficientMatrix dimension={dimension} />
+      <LoadingOverlay
+        active={isSolving}
+        spinner={<InProgressIcon />}
+        text="Solving equations..."
+      >
+        <CoefficientMatrix dimension={dimension} />
+      </LoadingOverlay>
       <BottomActionBar />
       <SolutionVariableList
         variables={[<SolutionVariable key="a" name={"a"} value={5.4} />]}
       />
       <SolvingFailedText />
-      <InProgressIcon />
     </main>
   );
 };
