35485722a068737f8ac527d7be080faf729608de
Andrin Meier <andrin@pricemeier.ch>
2022-05-30 19:51:05 +0200

    Remove netlify for now

--- webapp/remix.config.js
+++ webapp/remix.config.js
@@ -2,8 +2,8 @@
  * @type {import('@remix-run/dev').AppConfig}
  */
 module.exports = {
-  serverBuildTarget: "netlify",
-  server: "./server.js",
+  //serverBuildTarget: "netlify",
+  //server: "./server.js",
   cacheDirectory: "./node_modules/.cache/remix",
   ignoredRouteFiles: ["**/.*", "**/*.css", "**/*.test.{js,jsx,ts,tsx}"],
 };
