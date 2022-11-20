bbf3d8c9d3d656096874ee42f0fbf6330de25e8d
Andrin Meier <andrin@pricemeier.ch>
2022-11-20 13:50:57 +0100

    Add better explanation with an actual example

--- webapp/app/components/Explanation.tsx
+++ webapp/app/components/Explanation.tsx
@@ -1,8 +1,14 @@
 export const Explanation = () => {
   return (
     <div>
-      <p>This tool can be used to solve any system of linear equations.</p>
-      <p>Simply enter your equation into this matrix of coefficients.</p>
+        <p>This tool can be used to solve any system of linear equations.</p>
+        <p>Simply enter your equation into this matrix of coefficients.</p>
+        <p>Example equation:</p>
+        <p>x + 2y = 4</p>
+        <p>2x - 2y = 2</p>
+        <p>Would have you enter the coefficients:</p>
+        <p>First line: 1 (for x), 2 (for y) and 4 (for the right hand side.)</p>
+        <p>Second line: 2 (for x), -2 (for y) and 2 (for the right hand side.)</p>
     </div>
   );
 };
